8e8fc79d23aa03bccd0ae3526641b1f71a1303ea
Aphitchaya <98002786+Aphitchaya@users.noreply.github.com>
2024-02-09 05:43:08 +0700

    Create FloodManagePage

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../view/HomePage.vue';
-
+import HisTory from '../view/HisTory.vue';
+import FloodManage from '../view/FloodManage.vue';
 
 const routes = [
   {
     path: '/',
     name: 'HomePage',
     component: HomePage,
+  },
+
+  {
+    path: '/HisTory',
+    
+    name: 'HisTory',
+    component: HisTory,
+  },
+
+  {
+    path: '/FloodManage',
+    name: 'FloodManage',
+    component: FloodManage,
   },
   
   // ...more routes here
@@ -16,5 +30,7 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+
 
 export default router;
